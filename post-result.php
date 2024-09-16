390fe15cf4e6db0ed76204afe990926dcecc7f93
kalebtking <kalebtking@icloud.com>
2024-09-16 09:41:28 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -12,7 +12,7 @@ function getDisplay() {
  return "<p> The value sent is:</p>" . $_POST['my-name'];
 } else {
 
-  return "<p>Nothing posted to the page</p>"
+  return "<p>Nothing posted to the page</p>";
 }
 }
 ?>
